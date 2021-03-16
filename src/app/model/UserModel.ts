3e364bfa2f880210903624b463623685d27797cb
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-03-16 16:12:39 -0300

    Model and Bcrypt is ready

---
+++ src/app/model/UserModel.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, PrimaryGeneratedColumn, BeforeUpdate, BeforeInsert } from "typeorm";
+import bcrypt from 'bcryptjs'
+
+@Entity('User')
+export default class User {
+    @PrimaryGeneratedColumn('increment')
+    id: number
+
+    @Column('varchar', { length: 30 })
+    name: string
+
+    @Column('varchar', { length: 50 })
+    email: string
+
+    @Column('varchar', { length: 20 })
+    number: string
+
+    @Column('varchar', { length: 20 })
+    userName: string
+
+    @Column('varchar', { length: 50 })
+    password: string
+
+    
+    @BeforeInsert()
+    @BeforeUpdate()
+    passwordHash(){
+      this.password = bcrypt.hashSync(this.password, 8)
+    }
+}
